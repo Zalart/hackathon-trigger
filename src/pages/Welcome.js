42c05db7f85d805c2710b6dad00935ab20fad3a5
LapitskayaN <lapitskaya.nl@gmail.com>
2021-03-13 10:53:59 +0300

    add text and style wrapper

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,14 +1,15 @@
 import React from 'react'
-
 import { Link } from "react-router-dom";
 import { Button } from '@material-ui/core';
-
+import '../pages/Welcome.css';
 
 function Welcome() {
     return (
         <div className="welcome_page">
+           
             <div className="welcome_pic">
                 <div>
+                           
                     <p>
                         Some things should 
                     </p>
@@ -42,14 +43,22 @@ function Welcome() {
 
 
             <div className="welcome_text">
-                {/* <h1>Welcome page</h1> */}
+                <p>
+                    <span class="colortext"> 
+                         A Simple Act
+                    </span>
+                    </p> 
+                <b>that will change your life</b>
+                
+                <p>  
+                    Simple and powerful. Everything you need to start is available free on this website
+                </p> 
                 <p>
                     <span class="colortext"> Sometimes  </span>
                      we need someone to act in our favor.
                 </p>
-                <p> You can ask your friend, relative, or collegue.</p>
-                <p>  What happens if they forget to do something really important? You never know...</p>
-                <p> We’ll make sure that somthing will be done on time trigger, event or  periodically</p>
+                <h3> You can ask your friend, relative, or collegue.</h3>
+                <p>  What happens if they forget to do something really important? You never know... We’ll make sure that somthing will be done on time trigger, event or  periodically</p>
             </div>
                     
         </div>
