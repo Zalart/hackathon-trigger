f5ca86f9a39baba46fae1a51e9b6535e4cb769c1
artur <me@zalart.com>
2021-03-13 18:24:07 +0300

    main modification

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -66,38 +66,8 @@ const initialState = {
     //profilePicture: '',
     uiLoading: true,
     imageLoading: false
-
-import React from 'react';
-    import { Link } from "react-router-dom";
-    import LeftSide from "../components/leftSide";
-    import triggerCard from "../components/trigger/triggerCard";
-    import './Home.css';
-    import { Button } from '@material-ui/core';
-
-    function Home() {
-        return (
-            <div className="Home-Wrapper">
-                <div className="FieldPerson">
-                    <LeftSide />
-                    <Link>To welcome page</Link>
-                </div>
-                <div className="FieldTriggers">
-                    <div className="TriggerBut">
-                        <Button id="but-red"  >
-                            <Link to="/trigger">Add trigger</Link>
-                        </Button>
-                    </div>
-                    <div className="Triggercard">
-                        <br></br>
-                        <p>next </p>
-                        <p> trigger card</p>
-                        <triggerCard />
-                    </div>
-                </div>
-
-            </div>
-        )
 }
+
 function Home(props) {
     let history = useHistory();
     const [state, setState] = useState(initialState);
